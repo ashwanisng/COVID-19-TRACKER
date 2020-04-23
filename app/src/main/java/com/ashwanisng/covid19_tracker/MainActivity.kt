7dfbb8d67c29d12ee19339909c7ef62de0ee5b89
ashwanisng <ashwanicena5@gmail.com>
2020-04-23 23:33:00 +0530

    ui got updated

--- app/src/main/java/com/ashwanisng/covid19_tracker/MainActivity.kt
+++ app/src/main/java/com/ashwanisng/covid19_tracker/MainActivity.kt
@@ -2,13 +2,20 @@ package com.ashwanisng.covid19_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import okhttp3.Response
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+private val Response.statewise: Any
+    get() {}
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +32,51 @@ class MainActivity : AppCompatActivity() {
             if (response.isSuccessful){
 //                get the data from Json
                 val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                    bindCombinedData(data.statewise[0])
+                }
             }
+        }
+    }
+
+    private fun bindCombinedData(data: StatewiseItem) {
+
+//        now get the last updated time
+        val lastUpdatedTime = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        lastupdated_tv.text = "Last Updated\n ${getTimeAgo(simpleDateFormat.parse(lastUpdatedTime))}"
+    }
+
+//    now make a function to show the last updated time
+
+    fun getTimeAgo(past : Date): String {
+
+        val now = Date()
+
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+        val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+        return when {
+            seconds < 60 -> {
+                "Few seconds ago"
+            }
+
+            minutes < 60 -> {
+                "$minutes minutes ago"
+            }
+
+            hours < 24 -> {
+                "$hours hours ago"
+            }
+
+            else -> {
+
+                SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(past).toString()
+
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/ashwanisng/covid19_tracker/Response.kt
+++ app/src/main/java/com/ashwanisng/covid19_tracker/Response.kt
@@ -1,7 +1,7 @@
 package com.ashwanisng.covid19_tracker
 
 data class Response(
-	val statewise: List<StatewiseItem?>? = null
+	val statewise: List<StatewiseItem>
 )
 
 data class StatewiseItem(
